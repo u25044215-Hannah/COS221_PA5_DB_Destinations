6a826d5ac342f2eca5079f63846508baba73fd17
u25044215-Hannah <u25044215@tuks.co.za>
2026-05-20 10:10:20 +0200

    fixed the double call of the api, fixed the opening of a session
    Just call config once instead of config and api as it confuses the server and hence double calls the same functions

--- agency.php
+++ agency.php
@@ -3,12 +3,17 @@
 // Shelby Bodenstein
 // Tripistry - Agency, component, and group trip backend controller
 
-session_start();
+//checks if the session is already open, because it is called by multiple different things, it can open a session and then get stuck cause they don't close, hence the timeout
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 //gets the connection to the database
 require_once __DIR__ . "config.php";
-if (file_exists(__DIR__ . "api.php")) {
+
+//confusing the server, need to remove the double call of api
+/*if (file_exists(__DIR__ . "api.php")) {
     require_once __DIR__ . "api.php";
-}
+}*/
 
 //gives responses as JSON
 header("Content-Type: application/json; charset=utf-8");
