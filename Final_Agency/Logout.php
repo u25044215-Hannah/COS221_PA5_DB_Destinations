828d5cd123a431701ff5b82626e95936de3fb5b6
shelbybodenstein <u25038967@tuks.co.za>
2026-05-25 19:33:14 +0200

    Delete Final_Agency directory

--- Final_Agency/Logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) session_start();
-session_destroy();
-header("Content-Type: application/json; charset=utf-8");
-echo json_encode(["success" => true, "message" => "Logged out."]);
